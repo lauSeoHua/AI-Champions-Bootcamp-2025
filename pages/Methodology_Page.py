c378e8cf6f5289d48f7ce494a673396bdbd0a601
lauSeoHua <winniethepoohseohua@gmail.com>
2025-08-05 22:35:26 +0800

    edited_methodology_page_2

--- pages/Methodology_Page.py
+++ pages/Methodology_Page.py
@@ -31,7 +31,7 @@ with st.container():
         color: white;
         text-shadow: 0 0 5px rgba(0, 0, 0, 0.7);
     "> 
-    <div style="height: 100px;"></div> <!-- dummy content -->
+    <div style="height: 100px; width: 500px"></div> <!-- dummy content -->
     </div>
     """, unsafe_allow_html=True)
     st.write("References:\n 1. Python's Icon: Python Logo PNG Vector (SVG) Free Download. (2025). Seeklogo. https://seeklogo.com/vector-logo/332789/python \n 2. Neural Network's Icon: https://www.facebook.com/flaticon. (2019, September 3). Neural Icon - 2103633. Flaticon. https://www.flaticon.com/free-icon/neural_2103633 \n 3. Robot's Icon : https://www.facebook.com/flaticon. (2022, June 6). Robot Icon - 7717267. Flaticon. https://www.flaticon.com/free-icon/robot_7717267")
