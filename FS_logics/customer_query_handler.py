c053872ab366af6fc6b3f11ee3c0d27cbf00703e
lauSeoHua <winniethepoohseohua@gmail.com>
2025-08-09 19:11:51 +0800

    edited_get_effective_grouping_from_normalized_names_customer_query_handler

--- FS_logics/customer_query_handler.py
+++ FS_logics/customer_query_handler.py
@@ -92,8 +92,6 @@ def normalize_chemical_names(user_message):
         normalized_chemical_names_response_str = json.loads(normalized_chemical_names_response_str)
     except json.JSONDecodeError:
         normalized_chemical_names_response_str = []
-    st.write("Line 95")
-    st.write(normalized_chemical_names_response_str)
     return normalized_chemical_names_response_str
 
 def rag_find_best_match(normalized_name):
@@ -135,7 +133,7 @@ def rag_find_best_match(normalized_name):
     effective_grouping_response_str = llm_drugs.get_completion_by_messages(messages)
 
     #pretty-print the effective_grouping_match by removing double quotes and replace (";") or (",") with (" ")
-    if effective_grouping_match != "No effective grouping found":
+    if effective_grouping_response_str != "No effective grouping found":
         effective_grouping_match = effective_grouping_response_str.strip('"').replace(";",",")
 
     return (effective_grouping_match)
@@ -283,9 +281,7 @@ def search_poison_act_1938(normalized_name):
                     list_of_contexts.append(doc.page_content)
             except Exception as e:
                 print("Error at fallback query:", e, flush=True)
-    
-    st.write("Line 289")
-    st.write(list_of_contexts)
+
 
     with st.spinner("Cleaning 🧹🧹🧹 the matches... "):
         # Split by ; or whitespace, also add spacing for capitalized words stuck together
@@ -336,12 +332,7 @@ def search_poison_act_1938(normalized_name):
             else:
                 print("None")
                 conclusion = "None"
-        
-            st.write("Line 342")
-            st.write(conclusion)
     
-    st.write("Line 344")
-    st.write(possible_cpds)
 
     # Loop through possible_cpds list to search for words.
     if "".join([items for items in possible_cpds if items!= None]).strip()!="":
@@ -406,6 +397,5 @@ def get_effective_grouping_from_normalized_names(list_of_normalized_names):
     # All other random queries
     else:
         compiled_list.append("Sorry the application does not handle such queries currently. Maybe spelling error? Please correct spelling first. Thank you.")
-    st.write("Line 401")
-    st.write(compiled_list)
+
     return (compiled_list,tidied_list)
