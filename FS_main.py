b6b8475aa86a73d8f28f541b984792c3b74f0f60
lauSeoHua <winniethepoohseohua@gmail.com>
2025-08-11 20:45:28 +0800

    edited_FS_main

--- FS_main.py
+++ FS_main.py
@@ -290,7 +290,8 @@ Always consult with qualified professionals for accurate and personalized advice
                         st.write(compiled_list)
                         compiled_str = "\n".join(compiled_list)
                         st.write("Line 290")
-                        st.write(customer_query_handler.normalize_chemical_names(compiled_str))
+                        testlist = customer_query_handler.normalize_chemical_names(compiled_str)
+                        st.write(testlist)
                         output_response = customer_query_handler.get_effective_grouping_from_normalized_names(customer_query_handler.normalize_chemical_names(compiled_str))[0]
                         
                         for_writing_form = customer_query_handler.get_effective_grouping_from_normalized_names(customer_query_handler.normalize_chemical_names(compiled_str))[1]
