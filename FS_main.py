9740edb90b8e56de7b6a98fd0ad3437bffd08dae
lauSeoHua <winniethepoohseohua@gmail.com>
2025-08-11 20:39:02 +0800

    edited_FS_main

--- FS_main.py
+++ FS_main.py
@@ -287,6 +287,8 @@ Always consult with qualified professionals for accurate and personalized advice
                     if uploaded_file.type == "application/pdf":
                         compiled_list = read_library_search(uploaded_file).read_library_search()
                         compiled_str = "\n".join(compiled_list)
+                        st.write("Line 290")
+                        st.write(customer_query_handler.normalize_chemical_names(compiled_str))
                         output_response = customer_query_handler.get_effective_grouping_from_normalized_names(customer_query_handler.normalize_chemical_names(compiled_str))[0]
                         
                         for_writing_form = customer_query_handler.get_effective_grouping_from_normalized_names(customer_query_handler.normalize_chemical_names(compiled_str))[1]
