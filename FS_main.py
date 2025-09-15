2fed21d138845b87f2d0f91ecf4342d8ebc76fba
lauSeoHua <winniethepoohseohua@gmail.com>
2025-09-15 22:07:11 +0800

    test13

--- FS_main.py
+++ FS_main.py
@@ -292,8 +292,11 @@ Always consult with qualified professionals for accurate and personalized advice
                             tuple_of_list = customer_query_handler.get_effective_grouping_from_normalized_names(customer_query_handler.normalize_chemical_names(cpds.lower()))
                             output_response = tuple_of_list[0][0]
                             compiled_output_response = [].append(output_response)
-                            for_writing_form = tuple_of_list[1][0]
-                            for_writing_form_2.append(for_writing_form)
+                            if len(tuple_of_list[1])>0:
+                                for_writing_form = tuple_of_list[1][0]
+                                for_writing_form_2.append(for_writing_form)
+                            else:
+                                for_writing_form_2.append("")
                         # st.write("Line 289")
                         # st.write(compiled_list)
                         # compiled_str = "\n".join(compiled_list)
