3060f6008444b4b90976064e321dcbeec64d710b
lauSeoHua <winniethepoohseohua@gmail.com>
2025-10-27 07:15:02 +0800

    removed_st_write

--- FS_logics/customer_query_handler.py
+++ FS_logics/customer_query_handler.py
@@ -254,8 +254,8 @@ def search_poison_act_1938(normalized_name):
     give_id = []
    
     for chunk in (text_splitter_.split_text(search_result)):
-        st.write("Line 249")
-        st.write(chunk)
+        # st.write("Line 249")
+        # st.write(chunk)
         # Poisons Act 1938's drugs names are usually start with capital letter
         # If found the exact name , e.g. found exactly Sildenafil ->  found=True
         if normalized_name.capitalize() in chunk:
@@ -271,10 +271,10 @@ def search_poison_act_1938(normalized_name):
                 give_id.append(f"chunk {len(splitted_documents)}")
             except Exception as e:
                 print(f"Error appending chunk: {e}", flush=True)
-    st.write("Line 263")
-    st.write(list_of_contexts)
+    # st.write("Line 263")
+    # st.write(list_of_contexts)
     if len(list_of_contexts)==0:
-        search_result = tool_websearch.run(normalized_name.strip())
+        search_result = tool_websearch.run(normalized_name.strip().upper())
         for chunk in (text_splitter_.split_text(search_result)):
             st.write("Line 279")
             st.write(chunk)
